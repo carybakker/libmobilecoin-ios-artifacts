11f68c3d8fc7331dd63a48e00cd19f0cbc758f3d
Cary A. Bakker <cary@mobilecoin.com>
2022-08-10 15:35:30 -0400

    Snapshot build of task/ios-sdk-idempotence

---
+++ Artifacts/include/helloworld.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2018-2022 The MobileCoin Foundation
+
+#ifndef HELLO_WORLD_H_
+#define HELLO_WORLD_H_
+
+#include "common.h"
+
+/* ==================== Hello World ==================== */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool mc_hello_world();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HELLO_WORLD_H_ */
